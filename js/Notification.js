23e09f4ad7967f1559a8e5f5945dbc499a1c7231
segoncalves <segoncalves@hotmail.com.br>
2013-10-08 15:50:41 -0300

    Newest version of ScanQRCode
    App for learn Phonegap functionalities

---
+++ js/Notification.js
@@ -0,0 +1,54 @@
+﻿// Wait for device API libraries to load
+//
+document.addEventListener("deviceready", onDeviceReady, false);
+
+// device APIs are available
+//
+function onDeviceReady() {
+    // Empty
+}
+
+// alert dialog dismissed
+function alertDismissed() {
+    // do something
+}
+
+// Show a custom alertDismissed
+//
+function showAlert() {
+    navigator.notification.alert(
+        'You are the winner!',  // message
+        alertDismissed,         // callback
+        'Game Over',            // title
+        'Done'                  // buttonName
+    );
+}
+
+// process the promptation dialog result
+function onPrompt(results) {
+    alert("You selected button number " + results.buttonIndex + " and entered " + results.input1);
+}
+
+// Show a custom prompt dialog
+//
+function showPrompt() {
+    navigator.notification.prompt(
+        'Please enter your name',  // message
+        onPrompt,                  // callback to invoke
+        'Registration',            // title
+        ['Ok', 'Exit'],             // buttonLabels
+        'Jane Doe'                 // defaultText
+    );
+}
+
+// Beep three times
+//
+function playBeep() {
+    navigator.notification.beep(3);
+}
+
+// Vibrate for 2 seconds
+//
+function vibrate() {
+    navigator.notification.vibrate(2000);
+}
