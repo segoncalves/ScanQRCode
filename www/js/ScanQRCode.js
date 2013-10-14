9e01f6d1fbaad0cb10758910ba3a3e808f59e9e5
segoncalves <segoncalves@hotmail.com.br>
2013-10-14 17:01:31 -0300

    Testes

--- www/js/ScanQRCode.js
+++ www/js/ScanQRCode.js
@@ -79,5 +79,36 @@ var app = {
 
     TestesAlert: function () {
         alert('Chamada a função TestesAlert');
-    }
+    },
+
+    ScanBarcodeDemo: function () {
+        console.log('Funcao ScanBarcodeDemo. Calling...');
+
+        var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+
+        scanner.scan(function (result) {
+
+            alert("We got a barcode\n" +
+            "Result: " + result.text + "\n" +
+            "Format: " + result.format + "\n" +
+            "Cancelled: " + result.cancelled);
+
+            console.log("Scanner result: \n" +
+                 "text: " + result.text + "\n" +
+                 "format: " + result.format + "\n" +
+                 "cancelled: " + result.cancelled + "\n");
+
+            //
+            //document.getElementById("info").innerHTML = result.text;
+            console.log(result);
+            /*
+            if (args.format == "QR_CODE") {
+                window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
+            }
+            */
+
+        }, function (error) {
+            console.log("Scanning failed: ", error);
+        });
+    },
 };
